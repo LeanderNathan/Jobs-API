ef4c7e1283db8f4ce346deb271484c559d13ef39
Leander Nathan <leander.nathan@gmail.com>
2024-03-27 14:40:38 +0530

    Added Schema model for users

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name'],
+    minlength: 3,
+    maxlength: 30,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please prove a valid email',
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 6,
+    maxlength: 12,
+  },
+})
+
+module.exports = mongoose.model('User', UserSchema)
